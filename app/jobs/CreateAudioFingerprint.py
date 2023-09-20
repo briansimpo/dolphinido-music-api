3def2b1aad5d74be23fd4b89fc0ca92f69141a11
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-09-20 10:26:44 +0200

    Create CreateAudioFingerprint.py

---
+++ app/jobs/CreateAudioFingerprint.py
@@ -0,0 +1,20 @@
+import os
+from masonite.queues import Queueable
+from masonitedolphinido.dolphinido import Dolphinido
+from app.config.uploads import STORAGE_DIR
+from app.models import Song
+
+
+class CreateAudioFingerprint(Queueable):
+
+    def __init__(self, song: Song):
+        self.song = song
+
+    def handle(self):
+        dolphinido = Dolphinido()
+        audio_id = self.song.id
+        file_path = self.song.filepath
+        audio_file = os.path.realpath(os.path.join(STORAGE_DIR, file_path))
+        dolphinido.create_audio(audio_file, audio_id)
+
+
