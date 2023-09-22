561e97d7fc1b5b26322559212c629c02430695db
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-09-22 17:20:28 +0200

    Update PaginatorMixin.py

--- app/controllers/PaginatorMixin.py
+++ app/controllers/PaginatorMixin.py
@@ -3,7 +3,7 @@ from masonite.request import Request
 class PaginatorMixin:
 
 	def paginator(self, request: Request):
-		per_page = request.input('limit') or 20
+		per_page = request.input('limit') or 100
 		page = request.input('page') or 1
 		return int(per_page), int(page)
 
